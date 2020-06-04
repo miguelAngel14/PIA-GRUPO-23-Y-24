0cc067177df25a0d4e22022c728fd18b03edd45a
miguelAngel14 <61488303+miguelAngel14@users.noreply.github.com>
2020-06-04 00:00:36 -0500

    Create clasepia.py

---
+++ clasepia.py
@@ -0,0 +1,8 @@
+class CONTACTO:
+    def __init__(self,NickName,Nombre,Correo,Telefono,FechaNacimiento,Gasto):
+        self.NickName = NickName
+        self.Nombre= Nombre
+        self.Correo = Correo
+        self.Telefono= Telefono
+        self.FechaNacimiento = str(FechaNacimiento)
+        self.Gasto= Gasto
