7912b6ea38c56f471d86d818a25df8c8f02c1de8
miguelAngel14 <61488303+miguelAngel14@users.noreply.github.com>
2020-06-04 22:07:41 -0500

    Delete clasepia.py

--- clasepia.py
+++
@@ -1,8 +0,0 @@
-class CONTACTO:
-    def __init__(self,NickName,Nombre,Correo,Telefono,FechaNacimiento,Gasto):
-        self.NickName = NickName
-        self.Nombre= Nombre
-        self.Correo = Correo
-        self.Telefono= Telefono
-        self.FechaNacimiento = str(FechaNacimiento)
-        self.Gasto= Gasto
